88be71b8f2a9d8ae558166e0af5dcf5fcba0d19d
abdurrahmansagir21 <a.rahman.sagir@gmail.com>
2025-07-11 04:03:54 +0300

    update, delete, add and search with room

--- app/src/main/java/com/example/kisileruygulamasi/room/KisilerDao.kt
+++ app/src/main/java/com/example/kisileruygulamasi/room/KisilerDao.kt
@@ -1,7 +1,10 @@
 package com.example.kisileruygulamasi.room
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.kisileruygulamasi.data.entity.Kisiler
 
 @Dao
@@ -9,4 +12,17 @@ interface KisilerDao {
 
     @Query("SELECT * FROM kisiler")
     suspend fun kisileriYukle(): List<Kisiler>
+
+    @Insert
+    suspend fun kaydet(kisi: Kisiler)
+
+    @Update
+    suspend fun guncelle(kisi: Kisiler)
+
+    @Delete
+    suspend fun sil(kisi: Kisiler)
+
+    @Query("SELECT * FROM kisiler WHERE kisi_ad like '%'|| :aramaKelimesi||'%'")
+    suspend fun ara(aramaKelimesi: String): List<Kisiler>
+
 }
